abdac54161b866e85a384ca7c807f1b0c8309bad
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-03 01:25:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,9 +5,9 @@ class Student
     # create a new Student object given a row from the database
     new_student = self.new
     new_student.id = row[0]
-    new_student.name =  row[1]
+    new_student.name = row[1]
     new_student.grade = row[2]
-    new_song
+    new_student
   end
 
   def self.all
